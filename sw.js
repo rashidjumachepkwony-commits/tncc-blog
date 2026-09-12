b4e111e4c7cf8e8f75eea6d67eca777e00592d4e
Star Tech Africa <rashidjumachepkwomy@gmail.com>
2026-09-12 19:40:17 +0300

    fix dashboard flickering: prevent redirect loop, hide login until auth check, bump SW cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tncc-blog-v2';
+const CACHE_NAME = 'tncc-blog-v3';
 const APP_SHELL = ['./', './index.html', './manifest.json', './logo.jpeg'];
 
 self.addEventListener('install', event => {
